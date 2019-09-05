08859e4ae1b1870d68727de01e6bc4a79d3cc2a8
Frillion <fridirksoebech@gmail.com>
2019-09-05 15:09:48 +0000

    Found one article for the website

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,redirect
+
+links = ["Lögreglan hafði afskipti af nýnasistum"]
+hrefs = ["Logreglan_hafdi_afskipti__af_nynasistum"]
+Logreglan_hafdi_afskipti_af_nynasistum = {
+                                                "Title":"Lögreglan hafði afskipti af nýnasistum",
+                                                "content":"Félagsmenn í nýnasistasamtökunum Norðurvígi komu saman á "
+                                                "Lækjartorgi í Reykjavík í dag og dreifðu áróðri sínum. "
+                                                "Tilkynnt var um veru þeirra þar og mætti lögreglan á staðinn."
+                                                "Rafn Hilmar Guðmundsson, aðalvarðstjóri hjá lögreglu "
+                                                "höfuðborgarsvæðisins, staðfestir í samtali við mbl.is að "
+                                                "tilkynning hafi borist í þessum efnum og að lögregluþjónar "
+                                                "hafi verið sendir á staðinn. Þeir hafi rætt við mennina og "
+                                                "tekið niður nöfn þeirra."
+                                                "Dreifirit, sem mennirnir buðu vegfarendum að taka við, "
+                                                "er það sama og dreift var í íbúðarhús á Kársnesi í "
+                                                "Kópavogi í gær og hugsanlega víðar á höfuðborgarsvæðinu."
+                                                "Þar segir meðal annars að hugmyndafræði samtakanna sé "
+                                                "þjóðernisfélagshyggja sem er bein þýðing á þýska hugtakinu "
+                                                "„Nationalsozialismus“ sem aft­ur er sú stefna sem "
+                                                "þýskir nasistar undir forystu Adolfs Hitlers "
+                                                "stóðu fyrir á fyrri hluta síðustu aldar."
+                                                "Samtökin eru hluti af norrænni hreyfingu nýnasista.",
+                                                "Writer":"Arnþór",
+                                                "Date":"5.9.2019",
+                                                "Time of Creation": "13:15"
+                                        }
